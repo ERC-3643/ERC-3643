9edbf0ee00efa046d62ae3aa678c619a80655c29
Philippe Gonday <philippe.g@tokeny.com>
2025-10-07 08:08:38 +0200

    ✅ remove console logs

--- test/modules/test-module.test.ts
+++ test/modules/test-module.test.ts
@@ -81,8 +81,6 @@ describe('TestModule Multicall', () => {
       accounts: { deployer },
     } = await loadFixture(deployTestModuleFixture);
 
-    console.log('Here');
-
     const doSomething42Data = new Interface(['function doSomething(uint _value)']).encodeFunctionData('doSomething', [42]);
 
     await expect(testModule.connect(deployer).multicall([doSomething42Data])).to.be.revertedWithCustomError(testModule, 'OnlyBoundComplianceCanCall');
