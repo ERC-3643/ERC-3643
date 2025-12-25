0d67414b7e89a0fa63e7011edcfcb8711a3550b0
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2025-12-25 17:21:06 +0200

    ✅ coverage 100% token proxy

--- test/token/TokenInformation.t.sol
+++ test/token/TokenInformation.t.sol
@@ -13,8 +13,13 @@ import {
     UpdatedTokenInformation
 } from "contracts/ERC-3643/IERC3643.sol";
 import { IERC3643IdentityRegistry } from "contracts/ERC-3643/IERC3643IdentityRegistry.sol";
-import { EmptyString } from "contracts/errors/InvalidArgumentErrors.sol";
+import { MockContract } from "contracts/_testContracts/MockContract.sol";
+import { InitializationFailed } from "contracts/errors/CommonErrors.sol";
+import { DecimalsOutOfRange, EmptyString, ZeroAddress } from "contracts/errors/InvalidArgumentErrors.sol";
 import { ModularComplianceProxy } from "contracts/proxy/ModularComplianceProxy.sol";
+import { TokenProxy } from "contracts/proxy/TokenProxy.sol";
+import { ITREXImplementationAuthority } from "contracts/proxy/authority/ITREXImplementationAuthority.sol";
+import { TREXImplementationAuthority } from "contracts/proxy/authority/TREXImplementationAuthority.sol";
 import { IdentityRegistry } from "contracts/registry/implementation/IdentityRegistry.sol";
 import {
     AgentNotAuthorized,
@@ -501,6 +506,85 @@ contract TokenInformationTest is TokenTestBase {
         tokenImplementation.init(
             address(identityRegistry), address(complianceProxy), "Test Token", "TEST", 18, address(0)
         );
+    }
+
+    /// @notice Should revert when implementation authority is zero address
+    function test_TokenProxy_constructor_RevertWhen_ImplementationAuthorityZeroAddress() public {
+        address randomAddress = vm.addr(999);
+        vm.expectRevert(ZeroAddress.selector);
+        new TokenProxy(address(0), randomAddress, randomAddress, "Test", "TST", 18, address(0));
+    }
+
+    /// @notice Should revert when identity registry is zero address
+    function test_TokenProxy_constructor_RevertWhen_IdentityRegistryZeroAddress() public {
+        address randomAddress = vm.addr(999);
+        vm.expectRevert(ZeroAddress.selector);
+        new TokenProxy(address(getTREXImplementationAuthority()), address(0), randomAddress, "Test", "TST", 18, address(0));
+    }
+
+    /// @notice Should revert when compliance is zero address
+    function test_TokenProxy_constructor_RevertWhen_ComplianceZeroAddress() public {
+        address randomAddress = vm.addr(999);
+        vm.expectRevert(ZeroAddress.selector);
+        new TokenProxy(address(getTREXImplementationAuthority()), randomAddress, address(0), "Test", "TST", 18, address(0));
+    }
+
+    /// @notice Should revert when name is empty string
+    function test_TokenProxy_constructor_RevertWhen_NameEmpty() public {
+        ModularComplianceProxy complianceProxy = new ModularComplianceProxy(address(getTREXImplementationAuthority()));
+        address randomAddress = vm.addr(999);
+        vm.expectRevert(EmptyString.selector);
+        new TokenProxy(address(getTREXImplementationAuthority()), randomAddress, address(complianceProxy), "", "TST", 18, address(0));
+    }
+
+    /// @notice Should revert when symbol is empty string
+    function test_TokenProxy_constructor_RevertWhen_SymbolEmpty() public {
+        ModularComplianceProxy complianceProxy = new ModularComplianceProxy(address(getTREXImplementationAuthority()));
+        address randomAddress = vm.addr(999);
+        vm.expectRevert(EmptyString.selector);
+        new TokenProxy(address(getTREXImplementationAuthority()), randomAddress, address(complianceProxy), "Test", "", 18, address(0));
+    }
+
+    /// @notice Should revert when decimals is greater than 18
+    function test_TokenProxy_constructor_RevertWhen_DecimalsGreaterThan18() public {
+        ModularComplianceProxy complianceProxy = new ModularComplianceProxy(address(getTREXImplementationAuthority()));
+        address randomAddress = vm.addr(999);
+        vm.expectRevert(abi.encodeWithSelector(DecimalsOutOfRange.selector, 19));
+        new TokenProxy(address(getTREXImplementationAuthority()), randomAddress, address(complianceProxy), "Test", "TST", 19, address(0));
+    }
+
+    /// @notice Should revert when initialization fails (invalid implementation)
+    function test_TokenProxy_constructor_RevertWhen_InitializationFails() public {
+        // Deploy a mock contract that doesn't have init() function
+        MockContract mockImpl = new MockContract();
+
+        // Deploy an IA and manually set an invalid Token implementation
+        TREXImplementationAuthority incompleteIA = new TREXImplementationAuthority(true, address(0), address(0));
+
+        // Create a version with invalid Token implementation (mock contract without init())
+        ITREXImplementationAuthority.Version memory version =
+            ITREXImplementationAuthority.Version({ major: 4, minor: 0, patch: 0 });
+
+        ITREXImplementationAuthority.TREXContracts memory contracts = ITREXImplementationAuthority.TREXContracts({
+            tokenImplementation: address(mockImpl), // Invalid - doesn't have init() function
+            ctrImplementation: address(mockImpl), // Invalid
+            irImplementation: address(mockImpl), // Invalid
+            irsImplementation: address(mockImpl), // Invalid
+            tirImplementation: address(mockImpl), // Invalid
+            mcImplementation: address(mockImpl) // Invalid
+        });
+
+        // Add version to IA (need to be owner)
+        Ownable(address(incompleteIA)).transferOwnership(deployer);
+        vm.prank(deployer);
+        incompleteIA.addAndUseTREXVersion(version, contracts);
+
+        // Now try to deploy proxy - delegatecall to mockImpl.init() will fail
+        // because MockContract doesn't have init() function, causing InitializationFailed() revert
+        ModularComplianceProxy complianceProxy = new ModularComplianceProxy(address(getTREXImplementationAuthority()));
+        address randomAddress = vm.addr(999);
+        vm.expectRevert(InitializationFailed.selector);
+        new TokenProxy(address(incompleteIA), randomAddress, address(complianceProxy), "Test", "TST", 18, address(0));
     }
 
 }
